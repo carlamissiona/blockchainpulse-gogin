429c6c969a92a1dae17cab7e93e42f18cd191416
carlamissiona <missiona.carla@gmail.com>
2022-05-01 22:53:38 +0800

    Adde struct and indent

--- main.go
+++ main.go
@@ -4,6 +4,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 var Router * gin.Engine
+ 
+ //BLOCKPULSE_USER
+ //BLOCKPULSE_PRICE
+type bcap_user struct {
+    ID     string  `json:"id"`
+    Name  string  `json:"name"`
+    Email string  `json:"email"`    
+}
+
+type bcap_price struct {
+    ID     string  `json:"id"`
+    Name  string  `json:"name"`
+    Coin_Type string  `json:"coin_type"`
+    Price string  `json:"price"`
+}
+
+func init() {
+
+	var prices = []bcap_price{
+    {ID: "1", Name: "Dogge Coin", Coin_Type: "DGC", Price: "56.99"},
+    {ID: "2", Name: "Ethereum Coin", Coin_Type: "ETH", Price: "6.5"},
+	}
+}
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -11,5 +34,12 @@ func main() {
 			"message": "Hello world renter!",
 		})
 	})
+
+
+	r.GET("/api/blochain", func(c *gin.Context) {
+		 c.IndentedJSON(http.StatusOK, albums)
+	})
+
+
 	r.Run()
 }
